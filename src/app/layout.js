e39944f41ff3bb8bb8c0d932ab13afea6ad7b3ff
Samuel Moses A <56961772+SamuelMosesA@users.noreply.github.com>
2024-03-09 21:37:34 +0100

    Removed comments

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,15 +2,6 @@
 import './globals.css'
 import '../lib/data_functions'
 
-
-// export const metadata = {
-//   title: 'Bible Passage Selector',
-//   description: 'Tool to keep track of Bible Reading',
-//   icons: {
-//     icon: "/bible.png"
-//   }
-// }
-
 export default function RootLayout({children, params}) {
   return (
     <html lang="en">
